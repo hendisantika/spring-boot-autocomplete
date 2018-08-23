4ecf40d51f5e6f577e60bb329c66d6d5a81d7989
Hendi Santika <hendisantika@gmail.com>
2018-08-24 06:27:43 +0700

    #1 - Initial Project

---
+++ src/main/java/com/hendisantika/springbootautocomplete/SpringBootAutocompleteApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootautocomplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutocompleteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAutocompleteApplication.class, args);
+    }
+}
